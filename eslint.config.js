49d703f5a95d9ca7310a68ff5dcea474f825db93
charles <abcd854884@gmail.com>
2024-09-09 04:41:36 +0800

    📦 build: 修改 eslint、prettier

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import vue from 'eslint-plugin-vue';
+import ts from 'typescript-eslint';
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+    },
+  },
+
+  // js
+  js.configs.recommended,
+
+  // ts
+  ...ts.configs.recommended,
+
+  // vue
+  ...vue.configs['flat/recommended'],
+  {
+    // files: ['*.vue', '**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: ts.parser,
+      },
+    },
+  },
+  {
+    rules: {
+      eqeqeq: 'error',
+      camelcase: 'error',
+      'vue/eqeqeq': 'error',
+      'no-async-promise-executor': 'error',
+      'no-await-in-loop': 'error',
+      'no-promise-executor-return': 'error',
+      'require-atomic-updates': 'error',
+      'max-nested-callbacks': ['warn', 3],
+      'no-return-await': 'error',
+      'prefer-promise-reject-errors': 'error',
+      'prefer-const': 'error',
+    },
+  },
+];
