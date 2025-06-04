34e96737efd7c433ebae5c33cbc4bec2d41994ba
charles <abcd854884@gmail.com>
2025-06-05 04:14:38 +0800

    🧹 chore: 簡化 ESLint 設置

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,18 +1,13 @@
 import antfu from '@antfu/eslint-config';
 
 export default antfu({
-  stylistic: {
-    indent: 2,
-    quotes: 'single',
-  },
-
-  typescript: true,
   vue: true,
-  formatters: true,
+  typescript: true,
 
   ignores: [
     '**/*.md',
   ],
+
   rules: {
     '@stylistic/semi': ['error', 'always'],
     '@stylistic/member-delimiter-style': ['error', {
@@ -26,17 +21,12 @@ export default antfu({
       },
     }],
     'no-useless-escape': 'off',
-    'eqeqeq': 'error',
     'camelcase': 'error',
     'vue/eqeqeq': 'error',
-    'no-async-promise-executor': 'error',
     'no-await-in-loop': 'error',
-    'no-promise-executor-return': 'error',
     'require-atomic-updates': 'error',
     'max-nested-callbacks': ['warn', 3],
     'no-return-await': 'error',
-    'prefer-promise-reject-errors': 'error',
-    'prefer-const': 'error',
     'vue/singleline-html-element-content-newline': 'off',
     'vue/require-component-is': 'off',
     'vue/max-attributes-per-line': ['error', {
@@ -47,8 +37,6 @@ export default antfu({
         max: 1,
       },
     }],
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-    // 關閉衝突的規則，只保留 vue/script-indent
     'style/indent': 'off',
     'vue/script-indent': ['error', 2, { baseIndent: 1 }],
   },
