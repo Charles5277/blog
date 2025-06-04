913881d406d798e42e01774a0ec81fe7d66e71ec
charles <abcd854884@gmail.com>
2025-06-05 00:13:15 +0800

    🧹 chore: 調整 ESLint 規則

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,25 @@
-import antfu from '@antfu/eslint-config'
+import antfu from '@antfu/eslint-config';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default antfu({
-  ignores: ['**/*.md'],
+  ignores: ['**/*.md', '.vscode/**'],
+  typescript: true,
+  vue: true,
+  plugins: {
+    '@stylistic': stylistic,
+  },
   rules: {
+    '@stylistic/semi': ['error', 'always'],
+    '@stylistic/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'semi',
+        requireLast: true,
+      },
+      singleline: {
+        delimiter: 'semi',
+        requireLast: false,
+      },
+    }],
     'no-useless-escape': 'off',
     'eqeqeq': 'error',
     'camelcase': 'error',
@@ -16,7 +33,11 @@ export default antfu({
     'prefer-promise-reject-errors': 'error',
     'prefer-const': 'error',
     'vue/singleline-html-element-content-newline': 'off',
-    'vue/html-self-closing': 'off',
     'vue/max-attributes-per-line': 'off',
+    'vue/require-component-is': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    // 關閉衝突的規則，只保留 vue/script-indent
+    'style/indent': 'off',
+    'vue/script-indent': ['error', 2, { baseIndent: 1 }],
   },
-})
+});
